2eca1ea7c861a16b85879ee36015f517d74bf6df
ekoshiro16 <ekoshiro16@gmail.com>
2023-04-13 16:54:39 -0700

    finished strings page one component

--- src/components/js-content/strings/Strings.jsx
+++ src/components/js-content/strings/Strings.jsx
@@ -2,6 +2,178 @@ const Strings = (props) => {
   return (
     <div className="content">
       <h2>Strings</h2>
+      <h3>
+        Top 15 Most Important String Methods to Know for Technical Interviews
+      </h3>
+      <ul>
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt">
+            charAt()
+          </a>
+          <ul>
+            <li>Returns the character at a provided index position</li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat">
+            concat()
+          </a>
+          <ul>
+            <li>
+              Combines two or more provided strings and returns a new string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith">
+            endsWith()
+          </a>
+          <ul>
+            <li>
+              Determines whether a string ends with the characters of a provided
+              specific string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes">
+            includes()
+          </a>
+          <ul>
+            <li>
+              Determines whether a string contains the characters of a provided
+              specific string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf">
+            indexOf()
+          </a>
+          <ul>
+            <li>
+              Returns the index value of the first occurrence of a provided
+              specific string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf">
+            lastIndexOf()
+          </a>
+          <ul>
+            <li>
+              Returns the index value of the last occurrence of a provided
+              specific string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat">
+            repeat()
+          </a>
+          <ul>
+            <li>
+              Returns a new string which contains X repetitions of a provided
+              specific string, where X represents the number of times you would
+              like the input string to repeat.
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace">
+            replace()
+          </a>
+          <ul>
+            <li>
+              Returns a new string where some or all instances of a provided
+              string are replaced by another, provided string
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice">
+            slice()
+          </a>
+          <ul>
+            <li>
+              "Slices", or extracts, a section of a string and returns that
+              extraction as a new string.
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split">
+            split()
+          </a>
+          <ul>
+            <li>
+              Splits a string into an array of substrings by separating the
+              string based on a provided "separator" string.
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith">
+            startsWith()
+          </a>
+          <ul>
+            <li>
+              Determines whether a string begins with the characters of a
+              provided specified string.
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring">
+            substring()
+          </a>
+          <ul>
+            <li>
+              Returns the part of the string between a provided start and ending
+              indices value, or, if desired, to the end of the string entirely
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase">
+            toLowerCase()
+          </a>
+          <ul>
+            <li>Returns a given string as a fully lowercased string</li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase">
+            toUpperCase()
+          </a>
+          <ul>
+            <li>Returns a given string as a fully uppercased string</li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global">
+            trim()
+          </a>
+          <ul>
+            <li>Removes whitespace from both ends of a string</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
