d80043051a0b617fc1009d1d9574b0b222d80f18
ekoshiro16 <ekoshiro16@gmail.com>
2023-04-19 19:03:20 -0700

    refactor code editor component to be modular

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,39 +1,93 @@
 // Dependency imports
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-cobalt";
 
 const CodeEditor = (props) => {
-  const [editorContent, setEditorContent] = useState("");
+  const { defaultCode } = props;
+
+  const editorRef = useRef(null);
+  const outputEditorRef = useRef(null);
+
+  const [editorContent, setEditorContent] = useState(defaultCode);
+  const [output, setOutput] = useState("Output here...");
 
   const handleEditorChange = (value) => {
     setEditorContent(value);
   };
 
+  const handleExecuteClick = () => {
+    if (editorRef.current) {
+      const code = editorRef.current.editor.getValue();
+      const wrappedCode = `return (function() {${code}})();`;
+      const originalConsoleLog = console.log;
+      const logMessages = [];
+
+      console.log = (...args) => {
+        logMessages.push(args.join(" "));
+        originalConsoleLog.apply(console, args);
+      };
+
+      try {
+        const func = new Function(wrappedCode);
+        const result = func();
+
+        if (result !== undefined) {
+          console.log(result);
+        }
+
+        if (logMessages.length > 0) {
+          setOutput(logMessages.join("\n"));
+        } else {
+          setOutput("Nothing logged or executed");
+        }
+      } catch (error) {
+        console.error("Error executing code:", error);
+        setOutput(`Error: ${error.message}`);
+      } finally {
+        console.log = originalConsoleLog;
+      }
+    }
+  };
+
   return (
     <div>
-      <AceEditor
-        mode="javascript"
-        theme="monokai"
-        onChange={handleEditorChange}
-        name="editor"
-        editorProps={{ $blockScrolling: true }}
-        value={editorContent}
-        fontSize={14}
-        showPrintMargin={true}
-        showGutter={true}
-        highlightActiveLine={true}
-        setOptions={{
-          enableBasicAutocompletion: true,
-          enableLiveAutocompletion: true,
-          enableSnippets: true,
-          showLineNumbers: true,
-          tabSize: 2,
-        }}
-        width="100%"
-        height="400px"
-      />
+      <h2>Practice Problem</h2>
+      <div className="practice-cont">
+        <div className="editor-cont">
+          <AceEditor
+            mode="javascript"
+            theme="cobalt"
+            onChange={handleEditorChange}
+            name="editor"
+            editorProps={{ $blockScrolling: true }}
+            value={editorContent}
+            fontSize={12}
+            showPrintMargin={false}
+            showGutter={true}
+            wrapEnabled={true}
+            highlightActiveLine={true}
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: true,
+              showLineNumbers: true,
+              tabSize: 2,
+            }}
+            width="80%"
+            height="450px"
+            ref={editorRef}
+          />
+          <div className="output-cont">
+            <pre>{output}</pre>
+          </div>
+        </div>
+        <button className="execute-bttn" onClick={handleExecuteClick}>
+          Execute
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/js-content/arrays/ArraysTwo.jsx
+++ src/components/js-content/arrays/ArraysTwo.jsx
@@ -1,14 +1,7 @@
-// Dependency imports
-import React, { useRef, useState } from "react";
-import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/theme-cobalt";
+// Module imports
+import { CodeEditor } from "../../index";
 
 const ArraysTwo = (props) => {
-  const editorRef = useRef(null);
-  const outputEditorRef = useRef(null);
-
   const defaultCode = `/*
    PROMPT: Write an algorithm that reverses an array of strings (each representing a word) in place, then re-reverses each word into its original order.
    
@@ -23,85 +16,7 @@ const ArraysTwo = (props) => {
   */
   `;
 
-  const [editorContent, setEditorContent] = useState(defaultCode);
-  const [output, setOutput] = useState("Output here...");
-
-  const handleEditorChange = (value) => {
-    setEditorContent(value);
-  };
-
-  const handleExecuteClick = () => {
-    if (editorRef.current) {
-      const code = editorRef.current.editor.getValue();
-      const wrappedCode = `return (function() {${code}})();`;
-      const originalConsoleLog = console.log;
-      const logMessages = [];
-
-      console.log = (...args) => {
-        logMessages.push(args.join(" "));
-        originalConsoleLog.apply(console, args);
-      };
-
-      try {
-        const func = new Function(wrappedCode);
-        const result = func();
-
-        if (result !== undefined) {
-          console.log(result);
-        }
-
-        if (logMessages.length > 0) {
-          setOutput(logMessages.join("\n"));
-        } else {
-          setOutput("Nothing logged or executed");
-        }
-      } catch (error) {
-        console.error("Error executing code:", error);
-        setOutput(`Error: ${error.message}`);
-      } finally {
-        console.log = originalConsoleLog;
-      }
-    }
-  };
-
-  return (
-    <div>
-      <h2>Practice Problem</h2>
-      <div className="practice-cont">
-        <div className="editor-cont">
-          <AceEditor
-            mode="javascript"
-            theme="cobalt"
-            onChange={handleEditorChange}
-            name="editor"
-            editorProps={{ $blockScrolling: true }}
-            value={editorContent}
-            fontSize={12}
-            showPrintMargin={false}
-            showGutter={true}
-            wrapEnabled={true}
-            highlightActiveLine={true}
-            setOptions={{
-              enableBasicAutocompletion: true,
-              enableLiveAutocompletion: true,
-              enableSnippets: true,
-              showLineNumbers: true,
-              tabSize: 2,
-            }}
-            width="80%"
-            height="450px"
-            ref={editorRef}
-          />
-          <div className="output-cont">
-            <pre>{output}</pre>
-          </div>
-        </div>
-        <button className="execute-bttn" onClick={handleExecuteClick}>
-          Execute
-        </button>
-      </div>
-    </div>
-  );
+  return <CodeEditor defaultCode={defaultCode} />;
 };
 
 export default ArraysTwo;
