7d29017aafdbac1beaeecc63da1b57ec5740efe1
ekoshiro16 <ekoshiro16@gmail.com>
2023-04-24 18:08:21 -0700

    attempt to fix assets bug rd 2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
